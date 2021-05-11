20b84b3d329cfb53da007b688df09c4fc2ea465b
lantruong-2806 <tieuviemtu@gmail.com>
2021-05-11 17:16:45 +0700

    bai tap 1

---
+++ HOMEWORK_HOANVI/HOMEWORK_HOANVI/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void hoanvi(int *x, int *y);
+int main() {
+	int a, b;
+	a = 1;
+	b = 2;
+	hoanvi(&a, &b);// truyen vao dia chi cua a va b
+	printf("so a %x\r\n", a);
+	printf("so b %x\r\n", b);
+}
+void hoanvi(int* x, int* y) {
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+// bai tap hoan vi
